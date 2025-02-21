dfbaebf2ddeac756e79e51c7f734158a2b6110d0
Danielle Mesquita <danielle.cmesquita@gmail.com>
2025-02-20 23:24:40 -0300

    up

--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -2,6 +2,7 @@ import type {ReactNode} from 'react';
 import clsx from 'clsx';
 import Heading from '@theme/Heading';
 import styles from './styles.module.css';
+import Link from '@docusaurus/Link';
 
 type FeatureItem = {
   title: string;
@@ -11,37 +12,39 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Pesquisa',
+    Svg: require('@site/static/img/pequisa.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+         Pesquisas Sobre Padrões de Projetos.
+        <br />
+        <Link to="/docs/pesquisa">Saiba mais</Link>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'TCC',
+    Svg: require('@site/static/img/tcc.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+       Padrões de Projetos back-end web.
+        <br />
+        <Link to="/docs/tcc">Saiba mais</Link>
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Sobre o Aluno',
+    Svg: require('@site/static/img/sobre.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Aluna MBA - Engenharia de Software.
+        <br />
+        <Link to="/docs/sobre">Saiba mais</Link>
       </>
     ),
   },
 ];
-
 function Feature({title, Svg, description}: FeatureItem) {
   return (
     <div className={clsx('col col--4')}>
